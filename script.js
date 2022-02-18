b38f77d561f778912ad8cf6a178c3650153d7d7f
Michał <lmichal02@o2.pl>
2022-02-18 23:06:17 +0100

    prettier code

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const checkBtn = document.querySelector('.checkBtn')
 const result = document.querySelector('.result')
 const errorInfo = document.querySelector('.error')
 
-
 const braun = document.querySelector('.braun')
 const socialist = document.querySelector('.socialist')
 const corona = document.querySelector('.corona')
@@ -36,16 +35,11 @@ const countResult = () => {
 
 	const score = bmi - Number(hours.value) + Number(age.value) * 0.5
 
-
-
-    
-
 	if (yes.checked) {
 		newScore = score - 9
 	}
 
-
-	if(age.value < 25 && yes.checked) {
+	if (age.value < 25 && yes.checked) {
 		newScore = score + 0
 	}
 
@@ -67,25 +61,24 @@ const countResult = () => {
 	if (newScore > 43.01 && newScore < 50) {
 		result.textContent = 'Masz duże szanse, ciężko chorować na covid'
 	}
-	if (newScore > 50.01 ) {
+	if (newScore > 50.01) {
 		result.textContent = 'Masz niewielkie szanse, żeby przeżyć covida'
 	}
 
 	if (no.checked) {
-        newScore = 0
+		newScore = 0
 		resultImg.setAttribute('src', './img/niedzielski.jpg')
 		result.textContent = 'Niedzielski jest zawiedziony. Zostałeś umówiony na szczepienia na jutro!'
 	} else {
-        resultImg.setAttribute('src', "")
-        
-    }
+		resultImg.setAttribute('src', '')
+	}
 
 	if (braun.checked) {
 		newScore = 20
 		resultImg.setAttribute('src', './img/Grzegorz-Braun.jpg')
 		result.textContent =
 			'Możesz spać spokojnie. Braun ma cię pod opieką. Nigdy nie zarazisz się covidem. Z Panem Bogiem.'
-	} 
+	}
 
 	if (corona.checked) {
 		newScore = -100
@@ -93,15 +86,16 @@ const countResult = () => {
 			'Szanse, że umrzesz na covid wynoszą: 100%. Polecamy dalej oglądać ścieki medialne takie jak TVP i TVN.'
 		result.style.color = 'red'
 	}
-	if(socialist.checked) {
+	if (socialist.checked) {
 		newScore = -100
-		result.textContent = 'Według lewackiej ideologii: każdy jest równy i taki sam, dlatego każdy zachoruje na covida, włącznie z tobą :)'
+		result.textContent =
+			'Według lewackiej ideologii: każdy jest równy i taki sam, dlatego każdy zachoruje na covida, włącznie z tobą :)'
 	}
 
 	if (no.checked && corona.checked) {
 		resultImg.setAttribute('src', '')
 	}
-    
+
 	console.log(newScore.toFixed(2))
 }
 function onCheck() {
@@ -127,7 +121,7 @@ function unCheck() {
 
 const walidateForm = e => {
 	e.preventDefault()
-    
+
 	if (genders.some(gender => gender.checked)) {
 		genders.forEach(gender => gender.nextElementSibling.classList.remove('activeP'))
 		errors = 0
@@ -135,9 +129,6 @@ const walidateForm = e => {
 		genders.forEach(gender => gender.nextElementSibling.classList.add('activeP'))
 		errors++
 	}
-
-    
-
 
 	const yesNo = [yes, no]
 
@@ -159,29 +150,30 @@ const walidateForm = e => {
 		}
 	})
 
-    showError()
-	
+	showError()
 
-	if (errors === 0  && age.value >= 18 && age.value <= 120 && height.value >= 50 && height.value <= 250 && weight.value >= 20 && weight.value <= 200 && hours.value <= 100) {
-        countResult()
-        checkBtn.innerHTML = 'Reset'
+	if (
+		errors === 0 &&
+		age.value >= 18 &&
+		age.value <= 120 &&
+		height.value >= 50 &&
+		height.value <= 250 &&
+		weight.value >= 20 &&
+		weight.value <= 200 &&
+		hours.value <= 100
+	) {
+		countResult()
+		checkBtn.innerHTML = 'Reset'
 		checkBtn.classList.add('activeB')
-	} 
-	console.log(errors)
+	}
 
-	
-
-    if(result.textContent.length > 1) {
-        checkBtn.removeEventListener('click', walidateForm)
-    }
- 
-    
-    
+	if (result.textContent.length > 1) {
+		checkBtn.removeEventListener('click', walidateForm)
+	}
 }
 
 const showError = () => {
-
-	if(age.value < 18 || age.value >=120) {
+	if (age.value < 18 || age.value >= 120) {
 		errorInfo.textContent = 'Minimalny wiek to 18, a maksymalny 120'
 		age.classList.add('active')
 	} else if (height.value < 50 || height.value > 250) {
@@ -193,11 +185,9 @@ const showError = () => {
 	} else if (hours.value === '' && hours.value > 100) {
 		errorInfo.textContent = 'Maksymalna liczba godzin to 100'
 		hours.classList.add('active')
-	}else {
+	} else {
 		errorInfo.textContent = ''
 	}
-
-	
 }
 
 checkBtn.addEventListener('click', walidateForm)
